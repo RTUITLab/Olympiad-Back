bee4c34823b04ea7c889c69157d98310f609d6a0
Valentin <yuz-valya@yandex.ru>
2018-01-31 18:36:57 +0300

    Added Solution

---
+++ Models/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Solution
+    {
+        public string Language { get; set; }
+        public Guid Id { get; set; }
+        public string Raw { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ExerciseId { get; set; }
+    }
+}
