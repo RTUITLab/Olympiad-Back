8abd7855755f91ea9aa203fc4d5ffd24d5dce7cb
Valentin <yuz-valya@yandex.ru>
2018-02-01 16:34:16 +0300

    Finished CheckController Added ExerciseData

---
+++ Models/SolutionStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum SolutionStatus
+    {
+        InQueue,
+        InProcessing,
+        CompileError,
+        RunTimeError,
+        Sucessful,
+        WrongAnswer
+    }
+}
