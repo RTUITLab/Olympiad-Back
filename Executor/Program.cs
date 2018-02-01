59e9777dcc1d0c5489720f45e6943701cef23a66
Maksim <maksalmak@gmail.com>
2018-02-01 18:32:13 +0300

    Connected to database from executer

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Docker.DotNet;
-using Docker.DotNet.Models;
+using Microsoft.EntityFrameworkCore;
+using Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Executor
 {
@@ -12,9 +14,20 @@ namespace Executor
     {
         static async Task Main(string[] args)
         {
-            new DockerRunner().test();
-            Console.WriteLine("Program end");
-            Console.ReadLine();
+            var options = JsonConvert.DeserializeObject<JObject>(await File.ReadAllTextAsync("appsettings.Secret.json"));
+            var connectionString = options["ConnectionStrings"]["OlympDB"].ToString();
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            using (var db = new ApplicationDbContext(optionsBuilder.Options))
+            {
+                Console.WriteLine("Enter");
+                Console.WriteLine(db.Users.Count());
+                Console.WriteLine("Exit");
+            }
+
+
+
+                Console.ReadLine();
         }
     }
 }
