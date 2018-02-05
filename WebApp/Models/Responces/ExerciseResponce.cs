1ad58e89452bdaf49d7f0dbbf752ce62a29523e6
Valentin <yuz-valya@yandex.ru>
2018-02-05 17:55:55 +0300

    some new things

---
+++ WebApp/Models/Responces/ExerciseResponce.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Responces
+{
+    public class ExerciseResponce
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public SolutionStatus Status { get; set; }
+    }
+}
