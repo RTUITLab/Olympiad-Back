205007c4ee657ad47e3f0f2baec6de4959f1e3a9
Maksim <maksalmak@gmail.com>
2018-02-07 20:17:52 +0300

    adding overview text

---
+++ Executor/Executor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Executor
+{
+    class Executor
+    {
+        private ApplicationDbContext db;
+        private object connectionString;
+
+        public Executor(string dbConnectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(dbConnectionString);
+            db = new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
