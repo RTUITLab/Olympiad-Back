940775f0289adde17356f2c6c737f26f67c4b7f0
Maksim <maksalmak@gmail.com>
2018-02-07 21:48:55 +0300

    Added generic executor, page update last solution

---
+++ Executor/Executers/LanguageAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Executor.Executers
+{
+    [AttributeUsage(System.AttributeTargets.Class)]
+    class LanguageAttribute : Attribute
+    {
+        public string Lang { get; }
+        public LanguageAttribute(string lang)
+        {
+            Lang = lang;
+        }
+
+    }
+}
