12666c60e6917246dfbd332f58238af396ae48b2
Maksim <maksalmak@gmail.com>
2018-02-20 17:19:47 +0300

    End of rename, added python support

---
+++ Executor/Executers/Run/python/PythonRunner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Executor.Executers.Run.python
+{
+    [Language("python")]
+    class PythonRunner : ProgramRunner
+    {
+        public PythonRunner(Action proccessSolution) : base(proccessSolution)
+        {
+        }
+    }
+}
