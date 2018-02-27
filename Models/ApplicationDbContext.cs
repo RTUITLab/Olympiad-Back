4cee53625bb443a7d97cc0678724249f67af7e61
Valentin <yuz-valya@yandex.ru>
2018-02-27 14:03:33 +0300

    Added comment system

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Models
 {
+
     public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
@@ -16,5 +18,6 @@ namespace Models
         public DbSet<Exercise> Exercises { get; set; }
         public DbSet<Solution> Solutions { get; set; }
         public DbSet<ExerciseData> TestData { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
---
+++ WebApp/Controllers/CommentController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Comment")]
+    [Authorize(Roles = "User")]
+    public class CommentController : AuthorizeController
+    {
+        private readonly ApplicationDbContext context;
+
+        public CommentController(UserManager<User> userManager,
+            ApplicationDbContext context) : base(userManager)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostComment(IFormFile review)
+        {
+            string reviewBody;
+
+            if (review == null)
+            {
+                return BadRequest("Вы пытаетесь оставить пустой комментарий");
+            }
+
+            var stream = review.OpenReadStream();
+
+            using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+            {
+                reviewBody = await streamReader.ReadToEndAsync();
+            }
+
+            Comment comment = new Comment()
+            {
+                Raw = reviewBody,
+                UserId = UserId,
+                Time = DateTime.Now
+            };
+
+            await context.Comments.AddAsync(comment);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
