d655c0a8b228ba8430891d4700a859d3b4043706
Valentin <yuz-valya@yandex.ru>
2018-04-17 21:02:24 +0300

    editied Startup.cs

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -35,7 +35,6 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var c = Configuration.GetConnectionString("OlympDB");
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("OlympDB"),
                 b => b.MigrationsAssembly("WebApp")));
