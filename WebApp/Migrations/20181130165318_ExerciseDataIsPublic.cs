62499c08be5908803341e48aa6178a0d281bb58c
Maksim <maksalmak@gmail.com>
2018-11-30 20:58:34 +0300

    Update Backend, editing ExerciseData

---
+++ WebApp/Migrations/20181130165318_ExerciseDataIsPublic.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class ExerciseDataIsPublic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "TestData",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "TestData");
+        }
+    }
+}
