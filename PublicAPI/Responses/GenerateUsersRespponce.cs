1110a995aa8c957f9ca403cdc827928a2b73528e
Uzhakov Valentine <yuz-valya@yandex.ru>
2019-02-21 11:22:35 +0300

    add UserGenerate controller

---
+++ PublicAPI/Responses/GenerateUsersRespponce.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicAPI.Responses
+{
+    class GenerateUsersRespponce
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WebApp/Controllers/UserGenerateController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/UserGenerate")]
+    public class UserGenerateController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UserGenerateController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost("{challengeId:guid}")]
+        public async Task<IActionResult> GenerateUsers(Guid challengeId, List<string> studentIds)
+        {
+            List<User> generateUsers = new List<User>();
+
+            var challenge = dbContext.Challenges.Find(challengeId);
+
+            foreach (var studentId in studentIds)
+            {
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
