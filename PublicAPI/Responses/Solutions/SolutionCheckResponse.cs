9c5d4636a560e364ed762cab6bc3442b090638b0
Maksim <maksalmak@gmail.com>
2019-04-10 15:16:07 +0300

    Get users solutions of exercise

--- PublicAPI/Responses/Solutions/SolutionCheckResponse.cs
+++ PublicAPI/Responses/Solutions/SolutionCheckResponse.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace PublicAPI.Responses
+namespace PublicAPI.Responses.Solutions
 {
     public class SolutionCheckResponse
     {
