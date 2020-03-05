1ea6d996d6ff011191c4cae23f12405008c02e22
Maksim Makushchenko <maksalmak@gmail.com>
2020-03-05 15:24:24 +0300

    Updates for checking

--- Admin/Extensions.cs
+++ Admin/Extensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Olympiad.Admin
@@ -18,6 +20,12 @@ namespace Olympiad.Admin
                 .Where(e => e.ValueKind == JsonValueKind.Object)
                 .Where(d => d.TryGetProperty("stream", out _))
                 .Select(d => d.GetProperty("stream").GetString()));
+        }
+
+        private static readonly Regex reg = new Regex(@"(?i)\\[uU]([0-9a-f]{4})");
+        public static string ConvertCyrillic(this string value)
+        {
+            return reg.Replace(value, delegate (Match m) { return ((char)Convert.ToInt32(m.Groups[1].Value, 16)).ToString(); });
         }
     }
 }
