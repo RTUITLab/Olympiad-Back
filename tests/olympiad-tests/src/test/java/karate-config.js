32e4bedb5454f62b8e6c43f612c7fa9da858a3b3
Maksim Makushchenko <maksalmak@gmail.com>
2020-09-11 19:40:38 +0300

    Add first tests on Karate

---
+++ tests/olympiad-tests/src/test/java/karate-config.js
@@ -0,0 +1,20 @@
+function fn() {    
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    env: env,
+	baseUrl: 'http://127.0.0.1:5000'
+  }
+  var result = karate.callSingle('classpath:olympiad/auth.feature', config);
+  config.accessToken = 'Bearer ' + result.accessToken;
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+  } else if (env == 'e2e') {
+    // customize
+  }
+  return config;
+}
