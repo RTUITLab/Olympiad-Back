2dc38fc36ba1e17ffa6952978dabdc6933bfc787
Maksim Makushchenko <maksalmak@gmail.com>
2021-03-24 19:40:19 +0300

    Add zip logic

---
+++ ResultsViewer/wwwroot/js/ZipModule.js
@@ -0,0 +1,30 @@
+﻿
+class ZipPacker {
+    #zip;
+    constructor() {
+        this.#zip = new JSZip();
+    }
+    addFile(fileName, htmlContent) {
+        return new Promise((resolve, reject) => {
+            console.log(JSON.parse(htmlContent))
+            pdfMake.createPdf(JSON.parse(htmlContent)).open();
+
+            resolve();
+            //pdfMake.createPdf(JSON.parse(htmlContent)).getBlob(blob => {
+            //    this.#zip.file(`${fileName}.pdf`, blob);
+            //    resolve();
+            //});
+        })
+        
+    }
+    saveArchive() {
+        this.#zip.generateAsync({ type: "blob" })
+            .then(function (content) {
+                saveBlobAsFile("example.zip", content);
+            });
+    }
+}
+
+function createZipPacker() {
+    return new ZipPacker();
+}
