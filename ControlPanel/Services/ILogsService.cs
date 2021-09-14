338c1680f04587d35107b4422907d8a74820b9ed
Maksim Makushchenko <maksalmak@gmail.com>
2021-09-14 17:32:06 +0300

    Setup reading users file

--- ControlPanel/Services/ILogsService.cs
+++ ControlPanel/Services/ILogsService.cs
@@ -1,7 +1,12 @@
 ﻿
+using Microsoft.AspNetCore.Components;
+
 namespace Olympiad.ControlPanel.Services;
 public interface ILogsService
 {
     void Show();
     void LogInfo(string message, string? description = null);
+    void LogInfoObject<T>(T message);
+    void LogInfoObject<T>(string description, T message);
+    RenderFragment<string> OpenLogsButton { get; }
 }
