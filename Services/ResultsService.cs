17272d6c12cc7e09d706ff4a9b010dd89a8cf961
Maksim Makushchenko <maksalmak@gmail.com>
2021-11-02 00:02:09 +0300

    Rewrite challenge solutions results to one db request

--- Services/ResultsService.cs
+++ Services/ResultsService.cs
@@ -28,37 +28,44 @@ namespace Olympiad.Services
             var exercises = await db.Exercises
                 .OrderBy(e => e.ExerciseName)
                 .Where(e => e.ChallengeId == challengeId)
+                .Select(e => new ExerciseView
+                {
+                    ExerciseID = e.ExerciseID,
+                    ExerciseName = e.ExerciseName
+                })
                 .ToListAsync();
 
             var simpleData = await db
                     .Solutions
                     .Where(s => s.Exercise.ChallengeId == challengeId)
-                    .Include(s => s.User)
-                    .GroupBy(m => new { m.UserId, m.User.StudentID, m.ExerciseId })
+                    .GroupBy(m => new
+                    {
+                        m.UserId,
+                        m.User.StudentID,
+                        m.User.FirstName,
+                        m.ExerciseId
+                    })
+                    .OrderBy(s => s.Key.StudentID)
                     .Select(g => new
                     {
                         UserId = g.Key.UserId,
-                        StudentId = g.Key.StudentID,
+                        StudentID = g.Key.StudentID,
+                        FirstName = g.Key.FirstName,
                         ExerciseId = g.Key.ExerciseId,
                         Score = g.Max(s => s.TotalScore)
                     })
                     .ToListAsync();
+
+
             var userSolutions = simpleData
-                .GroupBy(s => new { s.UserId, s.StudentId })
+                .GroupBy(s => new { s.UserId, s.StudentID, s.FirstName })
                 .Select(g => new UserSolution
                 {
-                    UserId = g.Key.UserId,
-                    StudentId = g.Key.StudentId,
+                    User = new UserView { Id = g.Key.UserId, StudentId = g.Key.StudentID, FirstName = g.Key.FirstName },
                     Scores = g.ToDictionary(g => g.ExerciseId, g => g.Score)
                 })
-                .OrderBy(g => g.StudentId)
                 .ToList();
-            var targetIds = userSolutions.Select(u => u.UserId).ToArray();
-            var users = await db.Users.Where(u => targetIds.Contains(u.Id)).ToListAsync();
-            foreach (var usersolution in userSolutions)
-            {
-                usersolution.User = users.Single(u => u.Id == usersolution.UserId);
-            }
+
             return new ChallengeResults
             {
                 ChallengeName = challengeName,
@@ -70,15 +77,24 @@ namespace Olympiad.Services
         public class ChallengeResults
         {
             public string ChallengeName { get; set; }
-            public List<Exercise> Exercises { get; set; }
+            public List<ExerciseView> Exercises { get; set; }
             public List<UserSolution> UserSolutions { get; set; }
         }
+        public class ExerciseView
+        {
+            public Guid ExerciseID { get; set; }
+            public string ExerciseName { get; set; }
+        }
 
+        public class UserView
+        {
+            public Guid Id { get; set; }
+            public string StudentId { get; set; }
+            public string FirstName { get; set; }
+        }
         public class UserSolution
         {
-            public Guid UserId { get; set; }
-            public string StudentId { get; set; }
-            public User User { get; set; }
+            public UserView User { get; set; }
             public Dictionary<Guid, int?> Scores { get; set; }
         }
     }
