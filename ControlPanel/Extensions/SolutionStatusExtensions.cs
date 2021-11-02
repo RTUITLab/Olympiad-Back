804702d6e3b22fc03b48f9739ff64cb0ae7f049c
Maksim Makushchenko <maksalmak@gmail.com>
2021-11-02 21:55:59 +0300

    Show user compact solutions

---
+++ ControlPanel/Extensions/SolutionStatusExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Olympiad.Shared.Models;
+
+namespace Olympiad.ControlPanel.Extensions
+{
+    public static class SolutionStatusExtensions
+    {
+        public static string ToRussian(this SolutionStatus solutionStatus)
+        {
+            return solutionStatus switch
+            {
+                SolutionStatus.ErrorWhileCompile => "Ошибка системы при компиляции",
+                SolutionStatus.CompileError => "Ошибка при компиляции",
+                SolutionStatus.RunTimeError => "Ошибка во время исполнения",
+                SolutionStatus.WrongAnswer => "Неверный ответ",
+                SolutionStatus.TooLongWork => "Слишком долгое выполнение",
+                SolutionStatus.InQueue => "В очереди на проверку",
+                SolutionStatus.InProcessing => "Идет проверка",
+                SolutionStatus.Successful => "Решено верно",
+                _ => "Некорректный статус",
+            };
+        }
+    }
+}
