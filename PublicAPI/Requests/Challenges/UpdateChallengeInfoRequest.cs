298a7c85645c0d5f03835bb5efdaf87f1f7f30ef
Maksim Makushchenko <maksalmak@gmail.com>
2022-01-15 01:09:28 +0300

    Updated challenge model limitations

--- PublicAPI/Requests/Challenges/UpdateChallengeInfoRequest.cs
+++ PublicAPI/Requests/Challenges/UpdateChallengeInfoRequest.cs
@@ -1,4 +1,5 @@
-﻿using Olympiad.Shared.Models;
+﻿using Olympiad.Shared;
+using Olympiad.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,8 +10,9 @@ namespace PublicAPI.Requests.Challenges
     public class UpdateChallengeInfoRequest
     {
         [Required]
-        [MinLength(1), MaxLength(250)]
+        [MinLength(1), MaxLength(ChallengeLimitations.MAX_CHALLENGE_TITLE_LENGTH)]
         public string Name { get; set; }
+        [MaxLength(ChallengeLimitations.MAX_CHALLENGE_DESCRIPTION_LENGTH)]
         public string Description { get; set; }
         public DateTimeOffset? StartTime { get; set; }
         public DateTimeOffset? EndTime { get; set; }
