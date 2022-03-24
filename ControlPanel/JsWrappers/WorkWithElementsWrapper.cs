18d98c628aab1730ef8441f6b3a9d5df10af2a71
Maksim Makushchenko <maksalmak@gmail.com>
2022-03-25 01:08:03 +0300

    Setup markdown render, help with json file format, handle test groups file format,

--- ControlPanel/JsWrappers/WorkWithElementsWrapper.cs
+++ ControlPanel/JsWrappers/WorkWithElementsWrapper.cs
@@ -6,6 +6,14 @@ namespace Olympiad.ControlPanel.JsWrappers
 {
     public static class WorkWithElementsWrapper
     {
+        public static async ValueTask ScrollToElement(this IJSRuntime js, ElementReference? element)
+        {
+            if (element is null)
+            {
+                return;
+            }
+            await js.InvokeAsync<string>("scrollToSpecificElement", element);
+        }
         public static async ValueTask<string> GetInnerText(this IJSRuntime js, ElementReference? element)
         {
             if (element is null)
