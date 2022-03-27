7baaeb4693aaa2a049e95d79a2342bac8a52a61b
Maksim Makushchenko <maksalmak@gmail.com>
2022-03-27 06:28:15 +0300

    Added link to self page

--- ControlPanel/Pages/Routes.cs
+++ ControlPanel/Pages/Routes.cs
@@ -4,6 +4,10 @@ namespace Olympiad.ControlPanel.Pages;
 
 public static class Routes
 {
+    public static class Users
+    {
+        public static string UserEdit(Guid userId) => $"users/{userId}";
+    }
     public static class Challenges
     {
         public static string ChallengeEdit(Guid challengeId) => $"challenges/{challengeId}";
