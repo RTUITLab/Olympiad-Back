46ed4baf61f4ce0827431006fab4d27bb11820a4
Maksim Makushchenko <maksalmak@gmail.com>
2022-03-29 15:48:47 +0300

    Use dropdown on ChallengeResults

---
+++ ControlPanel/Components/DrowdownHelpers.cs
@@ -0,0 +1,8 @@
+﻿using AntDesign;
+
+namespace Olympiad.ControlPanel.Components;
+
+public static class DrowdownHelpers
+{
+    public static Trigger[] OnlyClickTrigger { get; } = new Trigger[] { Trigger.Click };
+}
