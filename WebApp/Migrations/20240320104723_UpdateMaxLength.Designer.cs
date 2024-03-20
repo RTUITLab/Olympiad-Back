b867dfdd7e1aa0fb40ead7b426881c467ef9886b
Maksim Makushchenko <maksalmak@gmail.com>
2024-03-20 13:47:57 +0300

    Add migration to update max length

--- WebApp/Migrations/20240320104723_UpdateMaxLength.Designer.cs
+++ WebApp/Migrations/20240320104723_UpdateMaxLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Models;
 using Models.Exercises;
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace WebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240320104723_UpdateMaxLength")]
+    partial class UpdateMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebApp/Migrations/20240320104723_UpdateMaxLength.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OutData",
+                table: "TestData",
+                type: "character varying(1000000)",
+                maxLength: 1000000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500000)",
+                oldMaxLength: 500000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InData",
+                table: "TestData",
+                type: "character varying(1000000)",
+                maxLength: 1000000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500000)",
+                oldMaxLength: 500000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OutData",
+                table: "TestData",
+                type: "character varying(500000)",
+                maxLength: 500000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000000)",
+                oldMaxLength: 1000000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InData",
+                table: "TestData",
+                type: "character varying(500000)",
+                maxLength: 500000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000000)",
+                oldMaxLength: 1000000);
+        }
+    }
+}
